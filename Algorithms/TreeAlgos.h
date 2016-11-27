78a2e829e815151ad89502ecd678ebf2e4fbfd74
schouthai <schouthai@hotmail.com>
2016-11-27 12:38:17 -0500

    Created by Coding Ground Client

---
+++ Algorithms/TreeAlgos.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+struct node {
+    int data;
+    node *left;
+    node *right;
+    
+    node(int _data) : data(_data), left(nullptttttr), right(nullptr) {} ;
+};
+
+node *head = nullptr;
+
+void testTreeAlgos() {
+
+    node* n1 = new node(1);
+    node* n2 = new node(1);
+    node* n3 = new node(1);
+    node* n1 = new node(1);
+    node* n1 = new node(1);
+    
+}
